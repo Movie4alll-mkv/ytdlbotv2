fde1ecbfdeb405e28e906134b8f96847a41bca24
ottoosworth <84224796+ottoosworth@users.noreply.github.com>
2021-12-09 10:26:49 +0300

    Res prefs

--- utils.py
+++ utils.py
@@ -64,7 +64,7 @@ def adjust_formats(user_id: "str", url: "str", formats: "list"):
     # high: best quality, 720P, 1080P, 2K, 4K, 8K
     # medium: 480P
     # low: 360P+240P
-    mapping = {"high": [], "medium": [480], "low": [240, 360]}
+    mapping = {"high": [1080], "medium": [720], "low": [480]}
     settings = get_user_settings(user_id)
     if settings and is_youtube(url):
         for m in mapping.get(settings[1], []):
